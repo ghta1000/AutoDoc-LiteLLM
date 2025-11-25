29248eda49fcdeadf169996e24b589478d4beb4c
Ghazal Tashakor <ghazaal22@gmail.com>
2025-11-25 03:59:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,3 @@
-#!/usr/bin/env bash
-
-# Simple helper script to run the AutoDoc-LiteLLM demo
-
-# Exit on first error
-set -e
-
-# Optional: create virtualenv (comment out if you don't want it)
-# python -m venv .venv
-# source .venv/bin/activate
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Running demo..."
+#!/bin/bash
+export OPENAI_API_KEY="YOUR_KEY_HERE"
 python main.py
-
